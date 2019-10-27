b73ca35e58dbb61f8bc20c960cc1dd619fe08981
Anudeep Gone <anudeepgone@gmail.com>
2019-10-27 19:25:58 +0800

    Create project structure

---
+++ PSISGApp/PSISGApp/Modules/View/Controller/PSIMapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PSISGApp
+//
+//  Created by Anudeep Gone on 27/10/19.
+//  Copyright © 2019 Anudeep. All rights reserved.
+//
+
+import UIKit
+
+class PSIMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
